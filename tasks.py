e192a31e64591f2a23a2f750a5420c9f65a1005b
pablomailo3000-boop <pablomailo3000@gmail.com>
2025-09-15 10:32:18 +0300

    fix: add .env

--- tasks.py
+++ tasks.py
@@ -1,8 +1,15 @@
 from invoke import task
+from dotenv import load_dotenv
 
+load_dotenv()
 
 @task
 def setup(ctx):
     ctx.run("uv python install 3.11.12", pty=True)
-    ctx.run("uv sync", pty=True)
-    
+    ctx.run("uv
+ sync", pty=True)
+
+
+@task
+def test(ctx):
+    ctx.run("echo $TEST_ENV", pty=True)
